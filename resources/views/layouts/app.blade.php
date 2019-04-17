9f787be4d83e15d450931e98f11b0d2354a9feda
nelsonbaez <bluexbaez@gmail.com>
2019-04-17 14:25:11 -0300

    Initial Commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8"> 
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>@yield('pagetitle')</title>
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/style.css') }}?version=1.1" rel="stylesheet">
+        <link rel="icon" href="{{ asset('http://www.friella.com.br/assets/outside/images/favicon.ico') }}" >
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+        <!-- icheck checkboxes -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/skins/square/yellow.css">
+
+        <!-- toastr notifications -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
+        
+        
+    </head>
+    <body>
+        <!-- CABEÇÁRIO -->
+        <header class="cinzinha">
+           
+        </header>
+        <!-- CONTEUDO -->
+        @yield('content')
+        <!-- RODAPÉ -->
+        <footer class="footer footer-color no-gutters ">
+            <div class="container">
+                
+            </div>
+        </footer>
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{ asset('/js/customjs.js') }}"></script>
+        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+        
+        <script>
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+        </script>
+        <!-- Scripts da Página-->
+        @yield('post-script')
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')    <!-- extende o codigo de Layout\app -->
+    @section('pagetitle', 'index')
+    @section('content')  <!-- define que o codigo dentro dessa variavel é o conteudo expresso na extenção -->
+        <main role="main">
+            <div class="container py-4 px-2 text-center"> 
+                <div class="row justify-content-around mt-5 ">
+                    a
+                </div>
+            </div>
+        </main>
+    @endsection
